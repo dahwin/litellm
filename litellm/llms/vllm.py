d7d08980c128c5522a26c08de8ee64fc942fb019
dahwin <116961692+dahwin@users.noreply.github.com>
2023-12-12 00:57:49 +0600

    Update vllm.py

--- litellm/llms/vllm.py
+++ litellm/llms/vllm.py
@@ -23,8 +23,7 @@ def validate_environment(model: str):
     try: 
         from vllm import LLM, SamplingParams # type: ignore
         if llm is None:
-            llm = LLM(model=model,dtype='float16',
-          trust_remote_code=True,tensor_parallel_size=1,worker_use_ray=1)
+            llm = LLM(model=model)
         return llm, SamplingParams
     except Exception as e:
         raise VLLMError(status_code=0, message=str(e))
